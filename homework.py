c2555452cda374c71190214fd65d6ec87b53cbe3
Evgeny Pozdnyakov <vz174@mail.ru>
2024-08-01 21:46:49 +0500

    tmp commit 1

--- homework.py
+++ homework.py
@@ -1,15 +1,22 @@
-...
+import logging
+import os
+import time
+import requests
+
+from dotenv import load_dotenv
+from telebot import TeleBot, types
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+# PAYLOAD = {'from_date': 1721301623}
 
 
 HOMEWORK_VERDICTS = {
@@ -20,7 +27,10 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    if (PRACTICUM_TOKEN is None
+            or TELEGRAM_CHAT_ID is None
+            or TELEGRAM_TOKEN is None):
+        return False
 
 
 def send_message(bot, message):
@@ -28,7 +38,13 @@ def send_message(bot, message):
 
 
 def get_api_answer(timestamp):
-    ...
+    homework_statuses = requests.get(
+        ENDPOINT,
+        headers=HEADERS,
+        params=timestamp).json()
+    return homework_statuses
+    # homeworks = homework_statuses[0].get('homeworks')
+    # current_date = homework_statuses[0].get('current_date')
 
 
 def check_response(response):
@@ -44,11 +60,14 @@ def parse_status(homework):
 def main():
     """Основная логика работы бота."""
 
+    #print(int(time.time()))
     ...
 
     # Создаем объект класса бота
-    bot = ...
+    bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
+    if not check_tokens():
+        bot.stop_polling()
 
     ...
 
