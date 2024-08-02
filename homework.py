86fe9425fd053e8c544ea97430bf65a890c1da43
PozdnyakovE <vz174@mail.ru>
2024-08-02 20:22:30 +0500

    Tmp commit 2

--- homework.py
+++ homework.py
@@ -32,54 +32,89 @@ def check_tokens():
             or TELEGRAM_TOKEN is None):
         return False
 
-
+# @bot.message_handler()
 def send_message(bot, message):
-    ...
+    bot.send_message(
+        chat_id=TELEGRAM_CHAT_ID,
+        text=message
+    )
 
 
 def get_api_answer(timestamp):
     homework_statuses = requests.get(
         ENDPOINT,
         headers=HEADERS,
-        params=timestamp).json()
+        params={'from_date': timestamp}).json()
     return homework_statuses
     # homeworks = homework_statuses[0].get('homeworks')
     # current_date = homework_statuses[0].get('current_date')
 
 
 def check_response(response):
-    ...
+    if ('homeworks' in response
+            and 'current_date' in response):
+        return True
 
 
 def parse_status(homework):
-    ...
-
+    status = homework.get('status')
+    verdict = HOMEWORK_VERDICTS.get(status)
+    homework_name = homework.get('homework_name')
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+
+
+# @bot.message_handler(commands=['start'])
+# def wake_up(message):
+#     chat = message.chat
+#     name = message.chat.first_name
+#     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#     button = types.KeyboardButton('/newcat')
+#     keyboard.add(button)
+
+#     bot.send_message(
+#         chat_id=TELEGRAM_CHAT_ID,
+#         text=f'Привет, {name}. Посмотри, какого котика я тебе нашёл',
+#         reply_markup=keyboard,
+#     )
 
 
 def main():
     """Основная логика работы бота."""
 
-    #print(int(time.time()))
-    ...
-
     # Создаем объект класса бота
     bot = TeleBot(token=TELEGRAM_TOKEN)
+    bot.polling()
     timestamp = int(time.time())
     if not check_tokens():
-        bot.stop_polling()
+        bot.stop_bot()
 
-    ...
+    # print(HEADERS)
+    # print(PRACTICUM_TOKEN)
+    # print(TELEGRAM_TOKEN)
+    #print(get_api_answer(1721301623))
+
+    current_status = ''
+    # api_answer = get_api_answer(1721301623)
+    # print(parse_status(api_answer.get('homeworks')[0]))
 
     while True:
         try:
-
-            ...
+            api_answer = get_api_answer(1721301623)
+            if check_response(api_answer):
+                homework = api_answer.get('homeworks')[0]
+            if current_status != parse_status(homework):
+                current_status = parse_status(homework)
+                send_message(
+                    bot,
+                    current_status
+                )
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             ...
-        ...
+        # time.sleep(RETRY_PERIOD)
+        time.sleep(20)
+    
 
 
 if __name__ == '__main__':
